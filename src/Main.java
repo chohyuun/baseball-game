902a422670ce30d78327ca22a524bf56b92b9d36
조현지 <qndjddl001@gmail.com>
2024-10-22 19:16:57 +0900

    feat: 결과 출력 구현
    입력받은 값을 토대로 결과 계산 후 출력
    - 답이 틀릴경우 맞을 때까지 계속 진행
    - AtomicInteger을 사용하여 람다 내에서 index 계산
    - 숫자 입력 시 범위를 넘어설 경우의 예외처리 개선

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,71 @@
 import baseballGame.BaseballGame;
 
-import java.util.ArrayList;
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
-    // todo: 입력 부 class 분리 또는 오류 발생시 재 입력 받을 수 있도록 변경 필요.
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         BaseballGame baseballGame = new BaseballGame();
-        ArrayList<Integer> result = new ArrayList<>(3);
 
-        baseballGame.randomNum();
+        HashSet<Integer> randomNum = baseballGame.randomNum();
 
-        try {
-            // 정답을 맞출 3개의 숫자를 입력 받기
-            while (result.size() < 3) {
-                System.out.print((result.size() + 1) + " 번째 숫자를 입력해 주세요: ");
 
-                int inputNum = scanner.nextInt();
+        while (true) {
+            Map<String, Integer> strikeBallCheck = new HashMap<>() {{
+                put("Strike", 0);
+                put("Ball", 0);
+            }};
+            ArrayList<Integer> result = new ArrayList<>(3);
+            AtomicInteger index = new AtomicInteger();
 
-                if (result.contains(inputNum) || inputNum == 0) {
-                    // 숫자를 중복 입력했을 때의 예외처리
-                    System.out.println("잘못된 입력입니다. 다시 입력해 주세요.(중복 숫자 또는 0은 입력될 수 없습니다)");
-                    continue;
+            try {
+                // 정답을 맞출 3개의 숫자를 입력 받기
+                while (result.size() < 3) {
+                    System.out.print((result.size() + 1) + " 번째 숫자를 입력해 주세요: ");
+
+                    int inputNum = scanner.nextInt();
+
+                    if (result.contains(inputNum) || inputNum < 1 || inputNum > 9) {
+                        // 숫자를 중복 입력했을 때의 예외처리
+                        System.out.println("잘못된 입력입니다. 다시 입력해 주세요.(중복 숫자, 1 - 9를 벗어난 수는 입력될 수 없습니다)");
+                        continue;
+                    }
+                    result.add(inputNum);
                 }
-
-                result.add(inputNum);
+            } catch (InputMismatchException e) {
+                System.out.println(e.getMessage());
             }
-        } catch (InputMismatchException e) {
-            System.out.println(e.getMessage());
+
+            System.out.println("" + result.get(0) + result.get(1) + result.get(2));
+
+            randomNum.forEach((number) -> {
+                if (Objects.equals(number, result.get(index.get()))) {
+                    // Strike 인지 체크
+                    strikeBallCheck.put("Strike", strikeBallCheck.get("Strike") + 1);
+                } else {
+                    // Strike 가 아니라면 Ball 인지 체크
+                    for (int i = 0; i < result.size(); i++) {
+                        if (Objects.equals(result.get(i), number)) {
+
+                            strikeBallCheck.put("Ball", strikeBallCheck.get("Ball") + 1);
+                            break;
+                        }
+                    }
+                }
+                index.set(index.get() + 1);
+            });
+
+            // 게임 종료 체크
+            if (strikeBallCheck.get("Strike") > 2) {
+                System.out.println("3 Strike! Game is over!");
+                break;
+            } else if (strikeBallCheck.get("Strike") != 0 || strikeBallCheck.get("Ball") != 0) {
+                System.out.println(strikeBallCheck.get("Strike") + " Strike, " + strikeBallCheck.get("Ball") + " Ball");
+            } else {
+                System.out.println("Out");
+            }
         }
     }
 }
