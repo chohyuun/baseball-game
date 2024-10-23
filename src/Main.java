7eeeb3c0c2a998d6a45d1296e7efeaf64f4718f6
조현지 <qndjddl001@gmail.com>
2024-10-23 20:05:50 +0900

    feat: 게임 난이도 조절 기능 추가
    Lv4 게임 난이도 기능 추가
    - 숫자를 입력받아 자릿수 지정 받음
    - 3, 4, 5이외의 자리수를 입력할 경우 예외처리 추가
    개행 수정

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,21 @@ import baseballGame.BaseballGame;
 
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         BaseballGame baseballGame = new BaseballGame();
-        HashSet<Integer> randomNum = baseballGame.randomNum();
+        HashSet<Integer> randomNum = baseballGame.randomNum(3);
         Map<Integer, Integer> gameCount = new HashMap<>();
 
         int count = 0;
         // 게임을 다시 시작했는지 확인하기 위한 변수
         boolean gameReset = true;
+
+        int digitNum = 3;
 
         while (true) {
             Map<String, Integer> strikeBallCheck = new HashMap<>() {{
@@ -21,23 +24,37 @@ public class Main {
                 put("Ball", 0);
             }};
 
-            ArrayList<Integer> result = new ArrayList<>(3);
+            ArrayList<Integer> result = new ArrayList<>(digitNum);
             AtomicInteger index = new AtomicInteger();
 
             String gameType;
 
-            if(gameReset) {
-                System.out.print("1. 게임 시작  2. 게임 기록 보기  3. 종료하기 \n입력: ");
+            if (gameReset) {
+                System.out.print("0. 자리수 설정(default: 3)  1. 게임 시작  2. 게임 기록 보기  3. 종료하기 \n입력: ");
 
                 gameType = scanner.nextLine();
 
-                if (Objects.equals(gameType, "3")) {
+                if (Objects.equals(gameType, "0")) {
+                    // 게임 난이도 지정
+                    System.out.print("설정하고자 하는 자리수를 입력 하세요(3, 4, 5 중 선택해 주세요.): ");
+                    String inputString = scanner.nextLine();
+
+                    if (Pattern.matches("[3-5]", inputString)) {
+                        // 3, 4, 5만 입력 가능
+                        digitNum = Integer.parseInt(inputString);
+                        randomNum = baseballGame.randomNum(digitNum);
+                    } else {
+                        System.out.println("자릿수를 다시 입력해주세요.");
+                        continue;
+                    }
+
+                } else if (Objects.equals(gameType, "3")) {
                     // 게임 종료
                     System.out.println("게임이 종료 됩니다! 감사!");
                     break;
                 } else if (Objects.equals(gameType, "2")) {
                     // 게임 기록 확인
-                    if(gameCount.isEmpty()){
+                    if (gameCount.isEmpty()) {
                         System.out.println("게임을 시작한 적이 없어 기록이 없습니다. 게임 시도 후 다시 확인해 주세요.");
                         continue;
                     }
@@ -74,7 +91,7 @@ public class Main {
                 }
             }
 
-            if (result.size() != 3 || inputResultNum >= 1000 || result.contains(0)) {
+            if (result.size() != digitNum || inputResultNum >= Math.pow(10, digitNum) || result.contains(0)) {
                 // 3자리가 아니거나 0이 포함 되었을 경우 예외처리
                 System.out.println("다시 입력해 주세요.");
                 continue;
@@ -101,10 +118,10 @@ public class Main {
                 index.set(index.get() + 1);
             });
 
-            count ++;
+            count++;
 
             // 게임 종료 체크
-            if (strikeBallCheck.get("Strike") > 2) {
+            if (strikeBallCheck.get("Strike") == digitNum) {
                 System.out.println("3 Strike! Game is over!");
                 gameReset = true;
                 scanner.nextLine();
--- src/baseballGame/BaseballGame.java
+++ src/baseballGame/BaseballGame.java
@@ -4,9 +4,9 @@ import java.util.HashSet;
 
 public class BaseballGame {
     // 정답이 될 랜덤 세자리 숫자 생성
-    public HashSet<Integer> randomNum() {
+    public HashSet<Integer> randomNum(int count) {
         HashSet<Integer> resultNum = new HashSet<>();
-        for (int i = 0; resultNum.size() < 3; i++) {
+        for (int i = 0; resultNum.size() < count; i++) {
             resultNum.add((int) (Math.random() * 9) + 1);
         }
         return resultNum;
