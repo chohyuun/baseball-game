4c28a35a45d4329d4cd5d0e076d678e5152276d4
조현지 <qndjddl001@gmail.com>
2024-10-28 15:20:19 +0900

    refactor: 게임 힌트용(Strike, Ball) Map을 정수형으로 수정
    - 자료형 정수형으로 수정
    - 수정 부분 README 내용 추가

--- src/baseballGame/BaseballGame.java
+++ src/baseballGame/BaseballGame.java
@@ -3,14 +3,11 @@ package baseballGame;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
-// todo: map 다른 형식으로 변환
 public class BaseballGame {
     private HashSet<Integer> randomNum = new HashSet<>();
-    public HashMap<String, Integer> strikeBallCheck = new HashMap<>(){{
-        put("Strike", 0);
-        put("Ball", 0);
-    }};
     private int tryCount = 0;
+    private int strikeCount = 0;
+    private int ballCount = 0;
 
     // 정답이 될 랜덤 세자리 숫자 생성
     public void createRandomNum(int count) {
@@ -23,16 +20,16 @@ public class BaseballGame {
     // Strike ball 개수 확인
     public void strikeBallCheck(ArrayList<Integer> result) {
         AtomicInteger index = new AtomicInteger();
-        strikeBallCheck.put("Strike", 0);
-        strikeBallCheck.put("Ball", 0);
+        strikeCount = 0;
+        ballCount = 0;
 
         randomNum.forEach((number) -> {
             if (Objects.equals(number, result.get(index.get()))) {
-                strikeBallCheck.put("Strike", strikeBallCheck.get("Strike") + 1);
+                strikeCount++;
             } else {
                 for (Integer integer : result) {
                     if (Objects.equals(integer, number)) {
-                        strikeBallCheck.put("Ball", strikeBallCheck.get("Ball") + 1);
+                        ballCount++;
                         break;
                     }
                 }
@@ -46,13 +43,13 @@ public class BaseballGame {
     public boolean gameEndCheck(int digitNum, HashMap<Integer, Integer> gameCount) {
         tryCount++;
 
-        if (strikeBallCheck.get("Strike") == digitNum) {
+        if (strikeCount == digitNum) {
             System.out.println("3 Strike! Game is over!");
             gameCount.put(gameCount.size() + 1, tryCount);
             tryCount = 0;
             return true;
-        } else if (strikeBallCheck.get("Strike") != 0 || strikeBallCheck.get("Ball") != 0) {
-            System.out.println(strikeBallCheck.get("Strike") + " Strike, " + strikeBallCheck.get("Ball") + " Ball");
+        } else if (strikeCount != 0 || ballCount != 0) {
+            System.out.println(strikeCount + " Strike, " + ballCount + " Ball");
         } else {
             System.out.println("Out");
         }
